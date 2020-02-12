8ae0d158e987833f018e5e2d0512cce9b415d187
anarcho34 <47782351+anarcho34@users.noreply.github.com>
2020-02-12 18:03:56 -0300

    Add files via upload
    suave na nave

---
+++ PRIMIERO PROJETO/src/primiero/projeto/PRIMIEROPROJETO.java
@@ -0,0 +1,19 @@
+
+package primiero.projeto;
+import javax.swing.JOptionPane;
+public class PRIMIEROPROJETO {
+
+
+    public static void main(String[] args) {
+        String firstNumber =
+                JOptionPane.showInputDialog("Enter first integer");
+        String secondNumber=
+                JOptionPane.showInputDialog("Enter second integer");
+        
+        int number1 = Integer.parseInt( firstNumber);
+        int number2 = Integer.parseInt( secondNumber);
+        
+                
+    }
+    
+}
